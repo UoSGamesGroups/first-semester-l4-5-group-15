9f1c3e24da469bce6de19127cf168d430790c715
CJace33 <s176047@UCS.ac.uk>
2016-11-09 19:03:00 +0000

    Starter code for file movement

---
+++ Code/Desk Job/Assets/Scripts/FileMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FileMover : MonoBehaviour {
+
+	    // Use this for initialization
+	    void OnMouseDown () {
+        Debug.Log("File clicked!");
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+	
+	    }
+    }
