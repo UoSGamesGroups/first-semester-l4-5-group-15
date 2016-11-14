82e6426817ca31d7cabe4bf50a635858469ec29e
CJace33 <s176047@UCS.ac.uk>
2016-11-14 16:53:49 +0000

    Drag and Drop features implamented

--- Code/Desk Job/Assets/Scripts/FileMover.cs
+++ Code/Desk Job/Assets/Scripts/FileMover.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FileMover : MonoBehaviour {
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System;
 
-	    // Use this for initialization
-	    void OnMouseDown () {
-        Debug.Log("File clicked!");
-	    }
-	
-	    // Update is called once per frame
-	    void Update () {
-	
-	    }
+public class FileMover : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public enum FileType
+    {
+        Circle,
+        Square,
+        Triangle
+    };
+
+    public static FileMover DraggedItem;
+
+    public FileType Type;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        DraggedItem = this;
+        GetComponent<Image>().raycastTarget = false;
     }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<Image>().raycastTarget = true;
+        DraggedItem = null;
+    }
+}
---
+++ Code/Desk Job/Assets/Scripts/FolderDrop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class FolderDrop : MonoBehaviour, IDropHandler {
+
+    public FileMover.FileType Type;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if ( FileMover.DraggedItem.Type == Type)
+        {
+            Debug.Log("You dragged it to the right place!");
+            Destroy(FileMover.DraggedItem.gameObject);
+            FileMover.DraggedItem = null;
+        }
+    }
+}
