5e4fbd1b8bc7d300b165b02204e64fce86367f04
CJace33 <s176047@UCS.ac.uk>
2016-11-21 14:28:04 +0000

    Added File instantiation and prefabs

---
+++ Code/Desk Job/Assets/Scripts/FilePosition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FilePosition : MonoBehaviour {
+
+    private RectTransform rectT;
+    private float width;
+    private float height;
+
+    void Start()
+    {
+        rectT = GetComponent<RectTransform>();
+        width = rectT.sizeDelta.x;
+        height = rectT.sizeDelta.y;
+    }
+
+	public void SetPosition(Vector2 size)
+    {
+        float left = -(size.x / 2);
+        float right = size.x / 2;
+        float top = -(size.y / 2);
+        float bottom = size.y / 2;
+
+        float xPos = Random.Range(left + width, right - width);
+        float yPos = Random.Range(top + height, bottom - height);
+
+        transform.localPosition = new Vector2(xPos, yPos);
+    }
+}
---
+++ Code/Desk Job/Assets/Scripts/LevelController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController : MonoBehaviour {
+
+    public int lvl;
+    public GameObject[] Prefabs;
+    public RectTransform CanvasTransform;
+
+	// Use this for initialization
+	void Start () {
+        lvl = 1;
+        LevelInit(lvl);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LevelInit (int Level)
+    {
+        for (int i = 0; i < (Level*30); i++)
+        {
+            GameObject newFile = Instantiate(Prefabs[Random.Range(0,Prefabs.Length)], transform.position, Quaternion.identity) as GameObject;
+            newFile.transform.SetParent(CanvasTransform);
+            newFile.GetComponent<FilePosition>().SetPosition(CanvasTransform.sizeDelta);
+        }
+    }
+}
