edadb12e92c60c8839104e3c4b28066c0b443eee
CJace33 <s176047@UCS.ac.uk>
2016-11-27 17:12:28 +0000

    Emails added to scene
    Not 100% implemented yet however

---
+++ Code/Desk Job/Assets/Scripts/emailController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class emailController : MonoBehaviour {
+
+    public GameObject [] emails;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < emails.Length; i++)
+        {
+            emails[i].gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
